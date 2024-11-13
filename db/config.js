2e7d7826a5205727b174e80e0ea23bc00af6ec38
BrunoPedro <46830056+BrunoPedro@users.noreply.github.com>
2024-11-13 22:52:03 +0000

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const connectionString = "<You'r string>";
+const connectionString = "<Your string>";
 const client = new MongoClient(connectionString);
 
 let db;
